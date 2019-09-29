c80342564170866751253c2732d777eb4048fd89
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-09-29 23:08:17 +0530

    Spring boot MVC jsp resolution
    https://howtodoinjava.com/spring-boot/spring-boot-jsp-view-example/

---
+++ src/main/java/com/faisal/spring/boot/springbootexp/web/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.faisal.spring.boot.springbootexp.web;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/hellojsp")
+	public String helloWorld(Map<String, Object> model) {
+		model.put("msg", "Hello World from JSP!");
+		return "helloWorld";
+	}
+
+	@RequestMapping("/greetingjsp")
+	public String greeting(Map<String, Object> model) {
+		model.put("msg", "Greetings from JSP!");
+		return "greeting";
+	}
+}
---
+++ src/main/webapp/WEB-INF/view/helloWorld.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+  pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Hello World</title>
+</head>
+<body>
+  <h1>${msg}</h1>
+  <a href="greetingjsp" title="Greeting JSP page">Go to greetings page</a>
+</body>
+</html>
