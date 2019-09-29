4f426d1c353967ef8569cc1c8525d9aea2fbbb05
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-09-29 16:25:55 +0530

    configuration property

--- src/main/java/com/faisal/spring/boot/springbootexp/web/HelloWorldRestController.java
+++ src/main/java/com/faisal/spring/boot/springbootexp/web/HelloWorldRestController.java
@@ -2,17 +2,30 @@ package com.faisal.spring.boot.springbootexp.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@ConfigurationProperties(prefix = "helloworld")
 public class HelloWorldRestController {
+	private String greeting;
 
 	@RequestMapping("/")
 	private String helloWorld() {
 		return "Hello World!";
 
+	}
+
+	@RequestMapping("/hello")
+	private String hello() {
+		return this.greeting;
+	}
+
+	// setting this public so that spring can set configuration property here
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
 	}
 
 	public static void main(String[] args) {
