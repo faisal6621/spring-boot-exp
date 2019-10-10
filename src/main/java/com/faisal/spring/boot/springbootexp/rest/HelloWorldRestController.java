7c48f688547da3a058fec6551bf500daf7112224
Mohammad Faisal <1015900+faisal6621@users.noreply.github.com>
2019-10-10 06:55:27 +0530

    added devtools dependency for hot-code replacement

--- src/main/java/com/faisal/spring/boot/springbootexp/rest/HelloWorldRestController.java
+++ src/main/java/com/faisal/spring/boot/springbootexp/rest/HelloWorldRestController.java
@@ -2,8 +2,8 @@ package com.faisal.spring.boot.springbootexp.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.faisal.spring.boot.springbootexp.configuration.HelloWorldConfiguration;
@@ -13,18 +13,18 @@ import com.faisal.spring.boot.springbootexp.configuration.HelloWorldConfiguratio
 @EnableConfigurationProperties(HelloWorldConfiguration.class)
 public class HelloWorldRestController {
 
-	@Autowired
-	private HelloWorldConfiguration configuration;
+    @Autowired
+    private HelloWorldConfiguration configuration;
 
-	@RequestMapping(method = RequestMethod.GET)
-	private String helloWorld() {
-		return "Hello World!";
+    @GetMapping
+    private String helloWorld() {
+        return "Hello World!";
 
-	}
+    }
 
-	@RequestMapping("/hello")
-	private String hello() {
-		return configuration.getGreeting();
-	}
+    @RequestMapping("/hello")
+    private String hello() {
+        return configuration.getGreeting();
+    }
 
 }
